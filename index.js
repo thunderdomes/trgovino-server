e01210873a57eac5644b5ed1beb8d0842ceb38d8
lateph <lateph@gmail.com>
2013-10-08 21:06:04 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var express = require('express');
 var schedule = require('node-schedule');
 var request = require("request");
 var app = express();
+var cronJob = require('cron').CronJob;
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'trgovino',
+});
+
+connection.connect(function(err) {
+	if(err){
+		console.log("database error : "+err);
+		process.exit(1);
+	}
+});
 
 /*
 
@@ -25,17 +41,59 @@ var getData = function(){
 		console.log("data updated");
 	});
 }
-var j = schedule.scheduleJob({hour: 01, minute: 00}, function(){
+
+var getLocalIndexInit = function(){
+	request("http://202.53.249.3/mi2/marketInfoData?request=localIndexInit", function(error, response, body) {
+		var str = body;
+		var arr = str.split("\n");
+		var data_simpan = [];
+		
+		var now = new Date();
+		var date = now.getFullYear()+'-'+ ('00'+(now.getMonth()+1)).slice (-2) +'-'+ ('00'+now.getDay()).slice (-2);
+		  date += ' '+('00'+now.getHours()).slice(-2)+':'+('00'+now.getMinutes()).slice(-2)+':'+('00'+now.getSeconds()).slice(-2);
+		
+		arr.forEach(function(e, i){
+			var row = JSON.parse(e || "null");
+			if(row != null){	
+				row.unshift(date)
+				data_simpan.push(row);	
+			}
+		});
+		var sql = "INSERT INTO index_local  VALUES ?";
+		connection.query(sql, [data_simpan], function(err) {
+			if(err){
+				console.log("penyimpanan error : "+err);
+				process.exit(1);
+			}
+		});
+		console.log("data updated");
+	});
+}
+
+var j = new cronJob('00 00 01 * * *', function(){
     getData();
-});
- 
+}, null, true);
+
 getData();
+
+var cronLocalIndexInit = new cronJob('*/13 * 9-17 * * 1-5', function(){
+    getLocalIndexInit();
+}, null, true);
 
 app.get('/', function(req, res){
   var body = JSON.stringify(_save_data || null);
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Content-Length', body.length);
   res.end(body);
+});
+
+app.get('/localIndexInit', function(req, res){
+	connection.query("select * from index_local where reqtime =  (select max(reqtime) from index_local)", function (err, rows, fields) {
+		var body = JSON.stringify(rows || null);
+		res.setHeader('Content-Type', 'application/json');
+		res.setHeader('Content-Length', body.length);
+		res.end(body);
+	});
 });
 
 app.get('/:id', function(req, res){
@@ -46,4 +104,4 @@ app.get('/:id', function(req, res){
   res.end(body);
 });
 
-app.listen(3000);
+app.listen(3234);
